f0373a56fa2ef3b6cdd59974241ba8113633a9bb
JInfantesC <jose.infantes@protonmail.com>
2020-02-12 21:38:34 +0100

    Templates for Hello module

--- plugins/hello/index.js
+++ plugins/hello/index.js
@@ -3,8 +3,8 @@ const { render } = require("server/reply");
 const r = require("../../utils/route_utils.js").getRoute("hello");
 
 module.exports = [
-	get(r(""), () => "Hello world!"),
-	get(r("hi"), (ctx) => render(__dirname + "/views/hello.pug", { name: ctx.query.name })),
+	get(r(""), () => render(__dirname + "/views/hello.pug", {})),
+	get(r("hi"), (ctx) => render(__dirname + "/views/hello_name.pug", { name: ctx.query.name })),
 	post(r(""), (ctx) => {
 		console.log(ctx.data);
 		return "ok";
